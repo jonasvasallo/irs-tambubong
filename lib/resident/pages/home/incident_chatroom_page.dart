539d5b5e9eb88fa498682db159ad291dd5d2c00b
Jonas Vasallo <iderrillix@gmail.com>
2024-05-14 04:14:29 +0800

    fix[tanod]: fixed overflow on chatroom

--- lib/resident/pages/home/incident_chatroom_page.dart
+++ lib/resident/pages/home/incident_chatroom_page.dart
@@ -48,120 +48,170 @@ class _IncidentChatroomPageState extends State<IncidentChatroomPage> {
       appBar: AppBar(
         title: Text("Chatroom"),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SingleChildScrollView(
-              child: StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection('incidents')
-                      .doc(widget.id)
-                      .collection('chatroom')
-                      .orderBy('timestamp', descending: false)
-                      .snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) {
-                      return Text('Error ${snapshot.error}');
-                    }
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                flex: 8,
+                child: SingleChildScrollView(
+                  child: StreamBuilder(
+                      stream: FirebaseFirestore.instance
+                          .collection('incidents')
+                          .doc(widget.id)
+                          .collection('chatroom')
+                          .orderBy('timestamp', descending: false)
+                          .snapshots(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) {
+                          return Text('Error ${snapshot.error}');
+                        }
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
 
-                    if (snapshot.data!.docs.isEmpty) {
-                      return Center(
-                        child: Text("No messages yet."),
-                      );
-                    }
+                        if (snapshot.data!.docs.isEmpty) {
+                          return Center(
+                            child: Text("No messages yet."),
+                          );
+                        }
 
-                    List<Widget> chatWidgets = [];
-                    final messages = snapshot.data?.docs.toList();
-                    for (var message in messages!) {
-                      final Widget chatWidget;
-                      if (message['sent_by'] ==
-                          FirebaseAuth.instance.currentUser?.uid) {
-                        chatWidget = Align(
-                          alignment: Alignment.centerRight,
-                          child: Padding(
-                            padding: EdgeInsets.only(top: 4, bottom: 4),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8),
-                                color: accentColor,
-                              ),
+                        List<Widget> chatWidgets = [];
+                        final messages = snapshot.data?.docs.toList();
+                        for (var message in messages!) {
+                          final Widget chatWidget;
+                          if (message['sent_by'] ==
+                              FirebaseAuth.instance.currentUser?.uid) {
+                            chatWidget = Align(
+                              alignment: Alignment.centerRight,
                               child: Padding(
-                                padding: EdgeInsets.all(8),
-                                child: Text(
-                                  message['content'],
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.white,
+                                padding: EdgeInsets.only(top: 4, bottom: 4),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    color: accentColor,
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsets.all(8),
+                                    child: Text(
+                                      message['content'],
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.white,
+                                      ),
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          ),
+                            );
+                          } else {
+                            chatWidget = FutureBuilder(
+                                future: FirebaseFirestore.instance
+                                    .collection('users')
+                                    .doc(message['sent_by'])
+                                    .get(),
+                                builder: (context, snapshot) {
+                                  if (!snapshot.hasData) {
+                                    return Text("No Message");
+                                  }
+                                  final userDetails = snapshot.data!;
+
+                                  return Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Padding(
+                                      padding:
+                                          EdgeInsets.only(top: 4, bottom: 4),
+                                      child: Row(
+                                        children: [
+                                          SizedBox(
+                                            height: 48,
+                                            width: 48,
+                                            child: ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(48),
+                                              child: Image.network(
+                                                userDetails['profile_path'],
+                                                fit: BoxFit.cover,
+                                              ),
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 8,
+                                          ),
+                                          Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                "${userDetails['first_name']} ${userDetails['last_name']}",
+                                                style: CustomTextStyle.regular,
+                                              ),
+                                              Container(
+                                                decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(8),
+                                                  color: minorText,
+                                                ),
+                                                child: Padding(
+                                                  padding: EdgeInsets.all(8),
+                                                  child: Text(
+                                                    message['content'],
+                                                    style:
+                                                        CustomTextStyle.regular,
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                });
+                          }
+                          chatWidgets.add(chatWidget);
+                        }
+                        return Column(
+                          children: chatWidgets,
                         );
-                      } else {
-                        chatWidget = Align(
-                          alignment: Alignment.centerLeft,
-                          child: Padding(
-                            padding: EdgeInsets.only(top: 4, bottom: 4),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8),
-                                color: minorText,
-                              ),
-                              child: Padding(
-                                padding: EdgeInsets.all(8),
-                                child: Text(
-                                  message['content'],
-                                  style: CustomTextStyle.regular,
-                                ),
-                              ),
-                            ),
-                          ),
-                        );
-                      }
-                      chatWidgets.add(chatWidget);
-                    }
-                    return Column(
-                      children: chatWidgets,
-                    );
-                  }),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  flex: 3,
-                  child: InputField(
-                    placeholder: "Type your message here...",
-                    inputType: "text",
-                    controller: _messageController,
+                      }),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: InputField(
+                      placeholder: "Type your message here...",
+                      inputType: "text",
+                      controller: _messageController,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  width: 8,
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 24),
-                    child: InputButton(
-                        label: "Send",
-                        function: () {
-                          sendMessage();
-                        },
-                        large: false),
+                  SizedBox(
+                    width: 8,
                   ),
-                ),
-              ],
-            ),
-          ],
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 24),
+                      child: InputButton(
+                          label: "Send",
+                          function: () {
+                            sendMessage();
+                          },
+                          large: false),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
