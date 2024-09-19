c673675c144c43354c8b989bf45b5948de20c629
Jonas Vasallo <iderrillix@gmail.com>
2024-09-19 16:47:21 +0800

    fix: wont navigate to login in ios - added platform check to subscribe to topic firebase messagign since it does not work in ios yet

--- lib/models/firebase_api.dart
+++ lib/models/firebase_api.dart
@@ -1,15 +1,21 @@
+import 'dart:io';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
 
   Future<String?> initNotifications() async {
-    await _firebaseMessaging.requestPermission();
-    await _firebaseMessaging.subscribeToTopic("incident-alert");
+    if (Platform.isAndroid) {
+      await _firebaseMessaging.requestPermission();
+      await _firebaseMessaging.subscribeToTopic("incident-alert");
 
-    final fcmToken = await _firebaseMessaging.getToken();
+      final fcmToken = await _firebaseMessaging.getToken();
 
-    print('Token: ${fcmToken}');
-    return fcmToken;
+      print('Token: ${fcmToken}');
+      return fcmToken;
+    } else {
+      return '';
+    }
   }
 }
